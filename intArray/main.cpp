e9ddd2503ad2241172d46dd16b548e40cf78483b
Nameless <tyrebrown873@gmail.com>
2020-04-08 13:50:22 -0400

    Test code

--- intArray/main.cpp
+++ intArray/main.cpp
@@ -3,10 +3,11 @@
 
 #include "intArray.h"
 #include <iostream>
+#include <random>
 
 int main()
 {
-    Nameless::intArray in(10);
+    Nameless::intArray in{ 10 };
 
     for (int i = 0; i < 10; i++)
     {
@@ -14,4 +15,37 @@ int main()
         std::cout << in[i] << '\n';
     }
     
+    std::cout << "\n\n";
+
+    Nameless::intArray copyTest{ in };
+
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << copyTest[i] << '\n';
+    }
+
+    std::cout << "\n\n";
+
+    Nameless::intArray assignmentTest{ 15 };
+
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> distribution(0, 100);
+
+    for (int i = 0; i < 15; i++)
+    {
+        assignmentTest[i] = distribution(rng);
+        std::cout << assignmentTest[i] << '\n';
+    }
+
+    std::cout << "\n\n";
+
+    copyTest = assignmentTest;
+
+    for (int i = 0; i < 15; i++)
+    {
+        std::cout << copyTest[i] << '\n';
+    }
+
+    std::cin.get();
 }
